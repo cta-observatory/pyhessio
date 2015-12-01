ea5a5e33188aa5dbd8228762fba83f33da3dbba7
Jean Jacquemier <jacquem@lapp.in2p3.fr>
2015-12-01 15:47:26 +0100

    Allow to pass hessio-prefix to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 from setuptools import setup, Extension
+import sys
 
-pyhessio_module = Extension(
-    'pyhessio.pyhessioc',
-    sources=['pyhessio/src/pyhessio.c'],
-    include_dirs = ['hessioxxx/include',  '.'],
-    libraries=['hessio'],
-    define_macros=[('CTA', None), ('CTA_MAX_SC', None)]
-)
+hessio_prefix=None
+
+# Search for hessio_prefix for local install
+if any("hessio_prefix" in s for s in sys.argv):
+  hessio_prefix = [s for s in sys.argv if 'hessio_prefix' in s][0]
+  sys.argv.remove(hessio_prefix)
+  hessio_prefix = hessio_prefix.split('=')[1]
+
+  pyhessio_module = Extension(
+      'pyhessio.pyhessioc',
+      sources=['pyhessio/src/pyhessio.c'],
+      include_dirs = ['hessioxxx/include',  '.'],
+      libraries=['hessio'],
+      library_dirs=[hessio_prefix],
+      runtime_library_dirs=[hessio_prefix],
+      define_macros=[('CTA', None), ('CTA_MAX_SC', None)]
+  )
+
+# else standars installation
+else:
+  pyhessio_module = Extension(
+      'pyhessio.pyhessioc',
+      sources=['pyhessio/src/pyhessio.c'],
+      include_dirs = ['hessioxxx/include',  '.'],
+      libraries=['hessio'],
+      define_macros=[('CTA', None), ('CTA_MAX_SC', None)]
+  )
+
 
 NAME = 'pyhessio'
 VERSION = '0.2'
@@ -28,3 +50,4 @@ setup(
     url=URL,
     ext_modules=[pyhessio_module],
 )
+
