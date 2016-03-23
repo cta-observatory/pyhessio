0dedc67b918b25d723318e1262d96cfe25d5d885
jacquemier <jacquem@lapp.in2p3.fr>
2016-03-23 17:40:25 +0100

    increment version 0.5.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ pyhessio_module = Extension(
 
 
 NAME = 'pyhessio'
-VERSION = '0.5.3'
+VERSION = '0.5.4'
 AUTHOR = 'cta developers'
 AUTHOR_EMAIL = 'jean.jacquemier@lapp.in2p3.fr'
 URL = 'https://github.com/cta-observatory/pyhessio'
